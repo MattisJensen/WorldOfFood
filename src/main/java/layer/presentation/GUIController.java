05be88eb77cd367489cb029db9776b573f09839f
JonasV321 <jonasv279@gmail.com>
2022-11-29 15:53:21 +0100

    møde

--- src/main/java/layer/presentation/GUIController.java
+++ src/main/java/layer/presentation/GUIController.java
@@ -1,14 +1,42 @@
 package layer.presentation;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import layer.domain.item.Item;
 
 public class GUIController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button collectButtom;
+
+    @FXML
+    private Button eatButtom;
+
+    @FXML
+    private ProgressBar foodBar;
+
+    @FXML
+    private GridPane gridPane;
+
+    @FXML
+    private ListView<Item> inventoryList;
+
+
+    @FXML
+    private Text scoreKlima;
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private TextField textField;
+
+    private ObservableList<Item> items;
 }
